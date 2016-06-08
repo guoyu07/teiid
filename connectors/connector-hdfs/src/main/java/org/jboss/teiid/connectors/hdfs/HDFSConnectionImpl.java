75e068c4e8ac7fdfab2a08a785a478e685d8adab
tugbadogan <tugbadogan92@gmail.com>
2016-06-08 18:45:00 +0300

    HDFS connector implementation

--- connectors/connector-hdfs/src/main/java/org/jboss/teiid/connectors/hdfs/HDFSConnectionImpl.java
+++ connectors/connector-hdfs/src/main/java/org/jboss/teiid/connectors/hdfs/HDFSConnectionImpl.java
@@ -3,12 +3,19 @@
  */
 package org.jboss.teiid.connectors.hdfs;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.resource.ResourceException;
 
-import org.teiid.resource.spi.BasicConnection;
-import org.teiid.logging.LogManager;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.teiid.core.BundleUtil;
 import org.teiid.logging.LogConstants;
+import org.teiid.logging.LogManager;
+import org.teiid.resource.spi.BasicConnection;
 
 /**
  * Connection to the resource. You must define HDFSConnection interface, that
@@ -20,12 +27,14 @@ public class HDFSConnectionImpl extends BasicConnection implements HDFSConnectio
 
 	private HDFSManagedConnectionFactory config;
 
-	public HDFSConnectionImpl(HDFSManagedConnectionFactory env) {
-		this.config = env;
-		// todo: connect to your source here
+	public HDFSConnectionImpl(HDFSManagedConnectionFactory config) throws URISyntaxException, IOException {
+		this.config = config;
 
+		Configuration conf = new Configuration();		
+		URI uri = new URI("hdfs://" + config.getHost() + ":" + config.getPort());
+		conf.set("fs.default.name", "hdfs://" + config.getHost() + ":" + config.getPort());
+		FileSystem fileSystem = FileSystem.get(uri, conf);
 		LogManager.logDetail(LogConstants.CTX_CONNECTOR, "HDFS Connection has been created."); //$NON-NLS-1$
-
 	}
 
 	@Override
--- connectors/connector-hdfs/src/main/java/org/jboss/teiid/connectors/hdfs/HDFSManagedConnectionFactory.java
+++ connectors/connector-hdfs/src/main/java/org/jboss/teiid/connectors/hdfs/HDFSManagedConnectionFactory.java
@@ -4,50 +4,51 @@
 package org.jboss.teiid.connectors.hdfs;
 
 import javax.resource.ResourceException;
-import javax.resource.spi.InvalidPropertyException;
-
-import org.teiid.resource.spi.BasicConnectionFactory;
-import org.teiid.resource.spi.BasicManagedConnectionFactory;
 
 import org.teiid.core.BundleUtil;
+import org.teiid.resource.spi.BasicConnectionFactory;
+import org.teiid.resource.spi.BasicManagedConnectionFactory;
 
 public class HDFSManagedConnectionFactory extends BasicManagedConnectionFactory {
 
 	public static final BundleUtil UTIL = BundleUtil.getBundleUtil(HDFSManagedConnectionFactory.class);
 
-	private String sampleProperty = null;
-	
+	private String host;
+	private Integer port;
+
 	@Override
 	public BasicConnectionFactory<HDFSConnectionImpl> createConnectionFactory() throws ResourceException {
 
-		if (sampleProperty == null) {
-	 		throw new InvalidPropertyException(UTIL.getString("HDFSManagedConnectionFactory.sampleproperty_not_set")); //$NON-NLS-1$
-		}
-		
 		return new BasicConnectionFactory<HDFSConnectionImpl>() {
 			@Override
 			public HDFSConnectionImpl getConnection() throws ResourceException {
 				return new HDFSConnectionImpl(HDFSManagedConnectionFactory.this);
 			}
 		};
-	}	
-	
-	// ra.xml files getters and setters go here.
-	public String getSampleProperty() {
-		return sampleProperty;
 	}
-	
-	public void setSampleProperty(String property) {
-		this.sampleProperty = property;
+
+	public String getHost() {
+		return host;
 	}
-		
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int prot) {
+		this.port = port;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime
-				* result
-				+ ((sampleProperty == null) ? 0 : sampleProperty.hashCode());
+		result = prime * result + ((host == null) ? 0 : host.hashCode());
+		result = prime * result + ((port == null) ? 0 : port.hashCode());
 		return result;
 	}
 
@@ -59,15 +60,21 @@ public class HDFSManagedConnectionFactory extends BasicManagedConnectionFactory 
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-			
+
 		HDFSManagedConnectionFactory other = (HDFSManagedConnectionFactory) obj;
 
-		if (!checkEquals(this.getSampleProperty(), other.getSampleProperty())) {
+		if (host == null) {
+			if (other.host != null)
+				return false;
+		} else if (!host.equals(other.host))
 			return false;
-		}
+		if (port == null) {
+			if (other.port != null)
+				return false;
+		} else if (!port.equals(other.port))
+			return false;
 
 		return true;
-
 	}
 
 }
